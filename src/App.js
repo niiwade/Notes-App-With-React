88a1e48233cced60e6fbbef3f65fe3838f8626f2
niiwade <niilantewade77@gmail.com>
2021-05-10 16:39:03 +0000

    pass note list so it can render each notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import NotesList from "./components/NotesList"
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+    id: nanoid(),
+    text: "1st",
+      date: "132441",
+    },
+
+    {
+      id: nanoid(),
+      text: "2nd",
+        date: "12344",
+    },
+    
+    {
+      id: nanoid(),
+      text: "3rd",
+        date: "12223",
+      }
+  ]);
   return (
     <div className="container">
-    <NotesList/>
+      <NotesList notes={notes}/>
   </div>
   );
 }
