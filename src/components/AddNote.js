628398b0800166627d88bae396e4bdccc21154bf
niiwade <niilantewade77@gmail.com>
2021-05-10 17:14:15 +0000

    update setNoteText value

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,8 +2,18 @@ import {useState} from 'react';
 
 const AddNote = () => {
     const [noteText, setNoteText] = useState('');
+
+    const handleChange = (event) => {
+        setNoteText(event.target.value);
+    }
+
     return (<div className="note new">
-        <textarea rows="8" cols="10" placeholder="Enter your Task/Idea..."></textarea>
+        <textarea
+            rows="8"
+            cols="10"
+            placeholder="Enter your Task/Idea..."
+            onChange={handleChange}
+        ></textarea>
         <div className="note-footer"><small>300 remaining</small></div>
         <button className="save">Save</button>
     </div>)
