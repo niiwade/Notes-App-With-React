4f977db305911f3e854daaca38aa35f975904311
niiwade <niilantewade77@gmail.com>
2021-05-10 17:10:50 +0000

    save note function

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,4 +1,7 @@
+import {useState} from 'react';
+
 const AddNote = () => {
+    const [noteText, setNoteText] = useState('');
     return (<div className="note new">
         <textarea rows="8" cols="10" placeholder="Enter your Task/Idea..."></textarea>
         <div className="note-footer"><small>300 remaining</small></div>
