a94b71b8cfa7887d9a5032c8551419c865c8a77c
niiwade <niilantewade77@gmail.com>
2021-05-10 16:55:55 +0000

    add a character count

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,5 +1,6 @@
 const AddNote = () => {
     return (<div className="note new">
         <textarea rows="8" cols="10" placeholder="Enter your Task/Idea..."></textarea>
+        <div className="note-footer"><small>300 remaining</small></div>
     </div>)
 }
