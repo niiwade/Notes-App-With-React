8da2f2279655b24625c0d3fa44339fff2abf8e85
niiwade <niilantewade77@gmail.com>
2021-05-10 16:59:02 +0000

    add save button to main div

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,5 +2,8 @@ const AddNote = () => {
     return (<div className="note new">
         <textarea rows="8" cols="10" placeholder="Enter your Task/Idea..."></textarea>
         <div className="note-footer"><small>300 remaining</small></div>
+        <button className="save">Save</button>
     </div>)
-}
+};
+
+export default AddNote;
