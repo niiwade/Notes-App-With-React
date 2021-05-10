d248f938b7abdeca35946e804518de15cbe7a936
niiwade <niilantewade77@gmail.com>
2021-05-10 16:48:13 +0000

    destructure props to rendered data

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,13 +1,13 @@
 import { MdDeleteForever } from 'react-icons/md';
 
-const Note = () => {
+const Note = ({id,text,date}) => {
     return (
         <div className="note">
 
-            <span>Hello, This is our Start</span>
+            <span>{text}</span>
 
             <div className="note-footer">
-                <small>12/2/2021</small>
+                <small>{date} </small>
                 <MdDeleteForever className='delete-icon' size='1.3em'/>
             </div>
         </div>
